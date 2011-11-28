b1b57a8532807392641d8e721939130ac5d3ecaa
Andrew Senior <aws@andrewsenior.com>
2011-11-27 21:05:29 -0500

    Add test namespace.

--- test/exiftest.cpp
+++ test/exiftest.cpp
@@ -25,57 +25,59 @@
 #include "redaction.h"
 
 namespace jpeg_redaction {
-int test_exif_removal(const char * const filename) {
-  try {
-    Jpeg j2;
-    bool success = j2.LoadFromFile(filename, true);
-    if (!success) exit(1);
-    j2.RemoveTag(TiffTag::tag_exif);
-    j2.Save("testout/test_noexif.jpg");
-  } catch (const char *error) {
-    fprintf(stderr, "Error: <%s> at outer level\n", error);
-    exit(1);
-  }
-  return 0;
-}
+  namespace tests {
+    int test_exif_removal(const char * const filename) {
+      try {
+	Jpeg j2;
+	bool success = j2.LoadFromFile(filename, true);
+	if (!success) exit(1);
+	j2.RemoveTag(TiffTag::tag_exif);
+	j2.Save("testout/test_noexif.jpg");
+      } catch (const char *error) {
+	fprintf(stderr, "Error: <%s> at outer level\n", error);
+	exit(1);
+      }
+      return 0;
+    }
 
-int test_sensitive_removal(const char * const filename) {
-  try {
-    Jpeg j2;
-    bool success = j2.LoadFromFile(filename, true);
-    if (!success) exit(1);
-    j2.RemoveAllSensitive();
-    j2.Save("testout/test_nosensitive.jpg");
-  } catch (const char *error) {
-    fprintf(stderr, "Error: <%s> at outer level\n", error);
-    exit(1);
-  }
-  return 0;
-}
+    int test_sensitive_removal(const char * const filename) {
+      try {
+	Jpeg j2;
+	bool success = j2.LoadFromFile(filename, true);
+	if (!success) exit(1);
+	j2.RemoveAllSensitive();
+	j2.Save("testout/test_nosensitive.jpg");
+      } catch (const char *error) {
+	fprintf(stderr, "Error: <%s> at outer level\n", error);
+	exit(1);
+      }
+      return 0;
+    }
 
-int test_gps_removal(const char * const filename) {
-  try {
-    Jpeg j2;
-    bool success = j2.LoadFromFile(filename, true);
-    if (!success) exit(1);
-    int rem = j2.RemoveTag(TiffTag::tag_exif);
-    if (rem != 1) throw("Didn't remove one exif tag");
-    rem = j2.RemoveTag(TiffTag::tag_gps);
-    if (rem != 1) throw("Didn't remove one gps tag");
-    j2.Save("testout/test_noexifgps.jpg");
-  } catch (const char *error) {
-    fprintf(stderr, "Error: <%s> at outer level\n", error);
-    exit(1);
-  }
-  return 0;
-}
+    int test_gps_removal(const char * const filename) {
+      try {
+	Jpeg j2;
+	bool success = j2.LoadFromFile(filename, true);
+	if (!success) exit(1);
+	int rem = j2.RemoveTag(TiffTag::tag_exif);
+	if (rem != 1) throw("Didn't remove one exif tag");
+	rem = j2.RemoveTag(TiffTag::tag_gps);
+	if (rem != 1) throw("Didn't remove one gps tag");
+	j2.Save("testout/test_noexifgps.jpg");
+      } catch (const char *error) {
+	fprintf(stderr, "Error: <%s> at outer level\n", error);
+	exit(1);
+      }
+      return 0;
+    }
+  } // namespace tests
 } // namespace jpeg_redaction
 
 int main(int argc, char **argv) {
   std::string filename("testdata/windows.jpg");
   if (argc > 1)
     filename = argv[1];
-  jpeg_redaction::test_exif_removal(filename.c_str());
-  jpeg_redaction::test_gps_removal(filename.c_str());
-  jpeg_redaction::test_sensitive_removal(filename.c_str());
+  jpeg_redaction::tests::test_exif_removal(filename.c_str());
+  jpeg_redaction::tests::test_gps_removal(filename.c_str());
+  jpeg_redaction::tests::test_sensitive_removal(filename.c_str());
 }
